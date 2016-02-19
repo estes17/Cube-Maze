7aaedfd5040b71fc9e9f5c60856dcc58542f9d29
Ashley Rosenberg <rosenbea17@up.edu>
2016-02-19 14:56:39 -0800

    Added invisible boundaries, camera follow, etc.
    The invisible walls keep the ball on the cube, the camera follows the player, and placed basic side walls and obstacles on one side

---
+++ Cube-Maze-master/3D Game/Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public float RotationSpeed = 10;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate ( (Input.GetAxis ("Mouse Y") * RotationSpeed * Time.deltaTime),
+			-(Input.GetAxis ("Mouse X") * RotationSpeed * Time.deltaTime),
+			0,
+			Space.World);
+		
+	}
+
+}
